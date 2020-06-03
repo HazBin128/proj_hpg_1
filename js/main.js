fa293ed20ce36b1cbaf63bb532b4ebd466c3efd0
HazBin <binhaz128@gmail.com>
2020-06-03 10:18:20 +0300

    pizdez

--- js/main.js
+++ js/main.js
@@ -1,14 +1,18 @@
 $(document).ready(function(){
 
+var height = $("#navigation").height();
+
   // рисуем хедер на весь экран, корректируем размер
- $('#header').height($(window).height() - $("#navigation").height());
+ $('#header').height($(window).height() - $("#navigation").height() - $("#description").height());
  $('main').css('top', $("#navigation").height());
 
 
- $(".navbar a").click(function(){
+ $(".nav-item a").click(function(){
  	$("body,html").animate({
- 		scrollTop:$("#" + $(this).data('value')).offset().top
+ 		scrollTop:$("#" + $(this).data('value')).offset().top - height
  	},1000)
+
+ $(".navbar-toggler").click();
 
  })
 
